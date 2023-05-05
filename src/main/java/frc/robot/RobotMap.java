ba9b6d8890e918cd798d91550fd9b83a7b9c0210
Bacon <brendenfreier951@gmail.com>
2023-05-05 15:25:10 -0400

    ids

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -3,10 +3,10 @@ package frc.robot;
 public class RobotMap {
     public static final int CONTROLLER_PORT = 0;
     public static class Motors {
-        public static final int FRONT_LEFT = 0;
+        public static final int FRONT_LEFT = 2;
         public static final int FRONT_RIGHT = 1;
-        public static final int BACK_LEFT = 2;
-        public static final int BACK_RIGHT = 3;
+        public static final int BACK_LEFT = 3;
+        public static final int BACK_RIGHT = 0;
     }
 
 }
